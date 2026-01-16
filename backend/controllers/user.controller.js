7eaa132895ba4cbeba57956d8be736e637e04d8a
nitaidalal <dalalnitai7@gmail.com>
2026-01-17 01:45:04 +0530

    implement updateProfile functionality with image upload and validation

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -24,3 +24,49 @@ export const suggestedUsers = async (req,res)=>{
         return res.status(500).json({message:"suggested users error", error} );
     }
 }
+
+
+export const updateProfile = async (req,res) => {
+    try {
+        const userId = req.userId;
+        const {name,userName, bio,gender} = req.body;
+
+        //check if username is taken by other user
+        const existingUser = await User.findOne({userName});
+        if(existingUser && existingUser._id.toString() !== userId){
+            return res.status(400).json({success:false, message:"Username is already taken"});
+        }
+
+        let profileImageUrl;
+        if(req.file){
+            const fileBase64 = req.file.buffer.toString("base64");
+            const fileUri = `data:${req.file.mimetype};base64,${fileBase64}`;
+            const uploadResult = await cloudinary.uploader.upload(fileUri, {
+              folder: "vibogram/profiles",
+              resource_type: "image",
+              transformation: [
+                { width: 400, height: 400, crop: "fill", gravity: "face" },
+                { quality: "auto" },
+                { fetch_format: "auto" },
+              ],
+            });
+            profileImageUrl = uploadResult.secure_url;
+        }
+
+        const updatedData = {
+            ...(name && {name}),
+            ...(userName && {userName}),
+            ...(bio && {bio}),
+            ...(gender && {gender}),
+            ...(profileImageUrl && {profileImage:profileImageUrl})
+        }
+        const updatedUser = await User.findByIdAndUpdate(userId, updatedData, {new:true}).select("-password");
+        if(!updatedUser){
+            return res.status(404).json({message:"User not found"});
+        }
+
+        return res.status(200).json({updatedUser});
+    } catch (error) {
+        return res.status(500).json({message:"Update profile error", error});
+    }
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import authMiddleware from '../middlewares/auth.middleware.js';
-import { getCurrentUser, suggestedUsers } from '../controllers/user.controller.js';
+import { getCurrentUser, suggestedUsers, updateProfile } from '../controllers/user.controller.js';
+import { imageUpload } from '../middlewares/multer.middleware.js';
 const userRouter = express.Router();
 
 userRouter.get("/current", authMiddleware, getCurrentUser);
 userRouter.get("/suggested", authMiddleware, suggestedUsers);
+userRouter.put("/update-profile", authMiddleware, imageUpload.single("profileImage"), updateProfile);
 export default userRouter;
