484eed24ef662d729f61cc941993a9fc491155f5
nitaidalal <dalalnitai7@gmail.com>
2026-02-25 22:32:31 +0530

    feat(story): update story routes to include getAllStories and correct getByUsername endpoint

--- backend/routes/story.routes.js
+++ backend/routes/story.routes.js
@@ -2,11 +2,12 @@ import express from "express";
 
 import authMiddleware from "../middlewares/auth.middleware.js";
 import {upload, handleMulterError} from "../middlewares/multer.middleware.js";
-import { getMyStory, uploadStory, viewStory } from "../controllers/story.controller.js";
+import { getAllStory, getStoryByUsername, uploadStory, viewStory } from "../controllers/story.controller.js";
 
 const storyRouter = express.Router();
 
 storyRouter.post("/upload", authMiddleware, upload.single("media"), handleMulterError, uploadStory);
 storyRouter.get("/view/:storyId", authMiddleware, viewStory);
-storyRouter.get("/view/user/:username", authMiddleware, getMyStory);
+storyRouter.get("/getByUsername/:username", authMiddleware, getStoryByUsername);
+storyRouter.get("/getAllStories", authMiddleware, getAllStory);
 export default storyRouter;
