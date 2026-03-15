7c58c2372ded025df43eee779dab812d8390691a
nitaidalal <dalalnitai7@gmail.com>
2026-03-16 01:13:57 +0530

    fix(cors): update CORS origin to production URL for secure access

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,12 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 //built-in middlewares
-app.use(cors({
-    origin:"http://localhost:5173 ",
-    credentials:true
-}));
+app.use(
+  cors({
+    origin: "https://vibely-social-media-app.vercel.app/",
+    credentials: true,
+  }),
+);
 app.use(
   express.json({
     limit: "50mb",
